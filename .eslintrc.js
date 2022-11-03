988d1c938fe4763e9a776cc8e472a792f2b091c1
David Jeong <drumrobot43@gmail.com>
2022-11-03 15:44:50 +0900

    2022-11-02

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module",
+  },
+  plugins: ["html"],
+  rules: {},
+};
