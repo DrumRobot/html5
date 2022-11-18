d535c1b8510c2865a791dffd689ee7c6a4f3eddb
David Jeong <drumrobot43@gmail.com>
2022-11-18 09:06:57 +0900

    2022-11-17

--- .vscode/launch.sh
+++ .vscode/launch.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-r=${1:(-5)}
-if [ $r = .html ]; then #  this is the snag
-    #export BROWSER=wslview
-    explorer.exe "http://127.0.0.1:3000/$1"
+if [ $fileExtname = .html ]; then
+  explorer.exe $url || true
 else
-    # do something txt-ish
-    echo node $1
+  node $file
 fi
